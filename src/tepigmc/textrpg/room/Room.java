d90a320eaae3063c7220520d07202a9ea7a72fe7
Andrew Archibald <andrew@robarchibald.com>
2015-05-10 18:06:59 -0700

    Add TileArray

--- src/tepigmc/textrpg/room/Room.java
+++ src/tepigmc/textrpg/room/Room.java
@@ -3,20 +3,21 @@ package tepigmc.textrpg.room;
 import java.util.List;
 
 import tepigmc.textrpg.tile.Tile;
+import tepigmc.textrpg.tile.TileArray;
 import tepigmc.util.SparseArray;
 
 public class Room {
-  private SparseArray<Tile> tiles;
+  private TileArray tiles;
   
-  public Room(SparseArray<Tile> tiles) {
+  public Room(TileArray tiles) {
     this.tiles = tiles;
   }
   
   public Room(Tile[][] tiles) {
-    this(new SparseArray<Tile>(tiles));
+    this(new TileArray(tiles));
   }
   
   public Room(List<List<Tile>> tiles) {
-    this(new SparseArray<Tile>(tiles));
+    this(new TileArray(tiles));
   }
 }
