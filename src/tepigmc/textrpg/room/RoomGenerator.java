29a7f6f84580835d37422e326cd00ecf22382765
Andrew Archibald <andrew@robarchibald.com>
2015-05-12 16:26:47 -0700

    Add RoomGenerator and Tile methods

---
+++ src/tepigmc/textrpg/room/RoomGenerator.java
@@ -0,0 +1,35 @@
+package tepigmc.textrpg.room;
+
+import java.util.HashMap;
+
+import tepigmc.textrpg.tile.Tile;
+import tepigmc.util.MatrixArray;
+
+public class RoomGenerator {
+  /**
+   * Creates a MatrixArray of Tile objects that can be used in a Room
+   * @param layout the layout of the room in Character objects
+   * @param hashMap the corresponding Tile objects for each Character
+   * @return a MatrixArray of Tile objects
+   */
+  public static MatrixArray<Tile> createTiles(String[] layout, HashMap<Character, Tile> hashMap) {
+    // Calculate the width the room will be
+    int mapWidth = 0;
+    int mapHeight = layout.length;
+    for (String row : layout) {
+      int length = row.length();
+      if (mapWidth < length) {
+        mapWidth = length;
+      }
+    }
+    // Fill the array with tiles corresponding to the Characters
+    Tile[][] tileMap = new Tile[mapHeight][mapWidth];
+    for (int r = 0; r < mapHeight; r++) {
+      String row = layout[r];
+      for (int c = 0; c < row.length(); c++) {
+        tileMap[r][c] = hashMap.get(row.charAt(c));
+      }
+    }
+    return new MatrixArray<Tile>(tileMap);
+  }
+}
--- src/tepigmc/textrpg/tile/Tile.java
+++ src/tepigmc/textrpg/tile/Tile.java
@@ -1,5 +1,30 @@
 package tepigmc.textrpg.tile;
 
 public abstract class Tile {
+  private String id;
+  private char icon;
+  private boolean isSolid;
+  
+  public Tile(String id, char icon, boolean isSolid) {
+    this.id = id;
+    this.icon = icon;
+    this.isSolid = isSolid;
+  }
+  
+  public String getId() { return this.id; }
+  public char getIcon() { return this.icon; }
+  public boolean getSolid() { return this.isSolid; }
 
+  public void setId(String id) { this.id = id; }
+  public void setIcon(char icon) { this.icon = icon; }
+  public void setSolid(boolean isSolid) { this.isSolid = isSolid; }
+
+  public boolean equals(Tile compare) {
+    if (this.getId() == compare.getId()
+        && this.getIcon() == compare.getIcon()
+        && this.getSolid() == compare.getSolid()) {
+      return true;
+    }
+    return false;
+  }
 }
