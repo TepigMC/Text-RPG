ac2c456db095b5ab4ed5e71de6afd538f5fd00dd
Andrew Archibald <andrew@robarchibald.com>
2015-05-14 17:12:02 -0700

    Added Tiles; Changed Tile to not be abstract

--- src/tepigmc/textrpg/tile/Tile.java
+++ src/tepigmc/textrpg/tile/Tile.java
@@ -1,24 +1,75 @@
 package tepigmc.textrpg.tile;
 
-public abstract class Tile {
+public class Tile {
   private String id;
   private char icon;
   private boolean isSolid;
   
+  /**
+   * Constructs a Tile object
+   * @param id the id to be used to identify Tile types
+   * @param icon the icon to be shown in the map screen
+   * @param isSolid whether the Tile is solid
+   */
   public Tile(String id, char icon, boolean isSolid) {
     this.id = id;
     this.icon = icon;
     this.isSolid = isSolid;
   }
   
-  public String getId() { return this.id; }
-  public char getIcon() { return this.icon; }
-  public boolean getSolid() { return this.isSolid; }
+  /**
+   * Gets the id used to identify Tile types
+   * @return id
+   */
+  public String getId() {
+    return this.id;
+  }
+  
+  /**
+   * Gets the icon shown in the map screen
+   * @return icon
+   */
+  public char getIcon() {
+    return this.icon;
+  }
+  
+  /**
+   * Gets if the Tile is solid
+   * @return isSolid
+   */
+  public boolean getSolid() {
+    return this.isSolid;
+  }
 
-  public void setId(String id) { this.id = id; }
-  public void setIcon(char icon) { this.icon = icon; }
-  public void setSolid(boolean isSolid) { this.isSolid = isSolid; }
+  /**
+   * Sets the id used to identify Tile types to the given id
+   * @param id
+   */
+  public void setId(String id) {
+    this.id = id;
+  }
+  
+  /**
+   * Sets the icon shown in the map screen to the given icon
+   * @param icon
+   */
+  public void setIcon(char icon) {
+    this.icon = icon;
+  }
+  
+  /**
+   * Sets if the Tile is solid to isSolid
+   * @param isSolid
+   */
+  public void setSolid(boolean isSolid) {
+    this.isSolid = isSolid;
+  }
 
+  /**
+   * Compares this and another Tile and returns whether the Tiles are equal
+   * @param compare the Tile to compare to
+   * @return if the Tiles are equal
+   */
   public boolean equals(Tile compare) {
     if (this.getId() == compare.getId()
         && this.getIcon() == compare.getIcon()
---
+++ src/tepigmc/textrpg/tile/Tiles.java
@@ -0,0 +1,6 @@
+package tepigmc.textrpg.tile;
+
+public class Tiles {
+  public static final Tile empty = new Tile("tile_empty", ' ', false);
+  public static final Tile wall = new Tile("tile_wall", '\u2588', true);
+}
